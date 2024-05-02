f927faa193c754bb3cf033a1657068d6e934d0dc
Lee Jae hwan <blue@nethru.co.kr>
2024-05-02 15:19:39 +0900

    프로듀서 테스트 추가

--- kafka_es/src/main/kotlin/io/study/kafka/producer/EventSender.kt
+++ kafka_es/src/main/kotlin/io/study/kafka/producer/EventSender.kt
@@ -2,11 +2,14 @@ package io.study.kafka.producer
 
 import io.micronaut.configuration.kafka.annotation.KafkaClient
 import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires
 import io.study.kafka.models.Event
 import jakarta.inject.Singleton
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
 
+
+@Requires(property = "spec.name", value = "EventSenderTest")
 @Singleton
 class EventSender(@param:KafkaClient private val kafkaProducer: Producer<Long, Event>,
                   @Property(name = "topic.producer") val topicName:String,
---
+++ kafka_es/src/test/kotlin/io/study/kafka/producer/EventSenderTest.kt
@@ -0,0 +1,18 @@
+package io.study.kafka.producer
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.core.util.StringUtils
+import io.study.kafka.models.Event
+import org.junit.jupiter.api.Test
+
+class EventSenderTest {
+
+    @Test
+    fun testSendEvents() {
+        ApplicationContext.run(mapOf("kafka.enabled" to StringUtils.TRUE, "spec.name" to "EventSenderTest"))
+            .use { ctx ->
+                val eventSender = ctx.getBean(EventSender::class.java)
+                eventSender.sendEvents(listOf(Event(1, "page_view", 1712642054000, "D1", null, 1)))
+            }
+    }
+}
