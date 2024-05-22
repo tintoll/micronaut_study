c5436a05e44e2bac8cd470d9cce68098eb4e43b6
Lee Jae hwan <blue@nethru.co.kr>
2024-05-22 10:57:14 +0900

    트랜잭션 추가

--- coroutine-r2dbc/src/main/kotlin/io/study/coroutine/PersonService.kt
+++ coroutine-r2dbc/src/main/kotlin/io/study/coroutine/PersonService.kt
@@ -2,11 +2,10 @@ package io.study.coroutine
 
 import jakarta.inject.Singleton
 import jakarta.transaction.Transactional
-import jakarta.validation.constraints.NotNull
 import kotlinx.coroutines.flow.toList
 
 @Singleton
-class PersonService(private val personRepository: PersonRepository) {
+open class PersonService(private val personRepository: PersonRepository) {
 
     suspend fun findAll(): List<Person> {
         return personRepository.findAll().toList()
@@ -16,8 +15,8 @@ class PersonService(private val personRepository: PersonRepository) {
         return personRepository.findById(id)
     }
 
-//    @Transactional(Transactional.TxType.MANDATORY)
-    suspend fun save(@NotNull person: Person): Person {
+    @Transactional
+    open suspend fun save(person: Person): Person {
         return personRepository.save(person)
     }
 
