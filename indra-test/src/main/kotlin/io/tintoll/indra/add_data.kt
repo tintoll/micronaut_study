5c2f47fff088d0e22d2a0e37d253eb7a9763ccf4
tintoll <belselios@gmail.com>
2025-01-22 11:31:48 +0900

    aa

--- indra-test/src/main/kotlin/io/tintoll/indra/add_data.kt
+++ indra-test/src/main/kotlin/io/tintoll/indra/add_data.kt
@@ -1,9 +1,12 @@
 package io.tintoll.indra
 
 import nethru.indra.indexer.Context
+import nethru.indra.range.InstantRange
 import nethru.indra.schema.registry.SchemaRegistry
 import nethru.indra.shard.ShardRegistry
+import java.time.Instant
 import java.util.concurrent.Executors
+import kotlin.system.exitProcess
 
 fun main() {
     val shardRegistry = ShardRegistry.open(tenantId, registryConfig)
@@ -21,4 +24,33 @@ fun main() {
 
     val shard = context.newShard(table)
     val indexer = context.indexer(shard)
+
+
+    eventLogs1().forEach {
+        val rowBuffer = indexer.buffer()
+        it.forEach { (k, v) ->
+            when (v) {
+                is String -> rowBuffer.token(k, v)
+                is Int -> rowBuffer.i64(k, v.toLong())
+                is Long -> rowBuffer.i64(k, v)
+            }
+        }
+        indexer.add(rowBuffer)
+    }
+
+    // 메타데이터
+    val mutableMetadata = shard.metadata.toMutable()
+    val start = Instant.ofEpochMilli(1726034470000)
+    val end = Instant.ofEpochMilli(17260362060000)
+    mutableMetadata.dateTimeRange("period", InstantRange(start, end))
+    indexer.bind(mutableMetadata)
+
+    val newShard = indexer.shardRef()
+    context.commit()
+    indexer.close()
+
+    checkDocs(registryConfig.shardPath, newShard)
+
+    println("insert - END")
+    exitProcess(0)
 }
